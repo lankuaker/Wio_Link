3022f56ed98f255aa559d9506dac32e6e8b6e64d
Jack Shao <jacky.shaoxg@gmail.com>
2015-08-21 23:50:46 +0800

    cleaning

--- users_build/local_user/grove_moisture_gen.h
+++
@@ -1,3 +0,0 @@
-#include "grove_moisture_class.h"
-
-void __grove_moisture_read_moisture(void *class_ptr, void *input);
--- users_build/local_user/grove_relay_gen.h
+++
@@ -1,3 +0,0 @@
-#include "grove_relay.h"
-
-void __grove_relay_write_onoff(void *class_ptr, void *input);
